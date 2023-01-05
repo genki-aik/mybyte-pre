fe3e9ded34e73645dabba3c04e7eef5f0b7ca6fe
Genki <genkima9@gmail.com>
2023-01-05 18:03:57 -0500

    Hide environmental variables

--- config/firebase.ts
+++ config/firebase.ts
@@ -5,15 +5,15 @@ import { getFirestore } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
+console.log(process.env.NEXT_PUBLIC_API_KEY)
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBf8FWFiohRg7hpO5u2GkqmhnAk0qSE4Z8",
-  authDomain: "firebsae-next-auth.firebaseapp.com",
-  projectId: "firebsae-next-auth",
-  storageBucket: "firebsae-next-auth.appspot.com",
-  messagingSenderId: "804445792649",
-  appId: "1:804445792649:web:288c903ebda73df4059958"
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID
 };
 
 // Initialize Firebase
